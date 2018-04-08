24eaf2bc3b799bf0c14ea060832c3348e65cf28b
Temigo <temigo@gmx.com>
2018-04-08 15:49:49 -0700

    Add CORS support

--- api.py
+++ api.py
@@ -1,46 +1,67 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_restful.utils import cors
 import json
+from flask.json import jsonify
+#from common.parser
 
 app = Flask(__name__)
 api = Api(app)
+api.decorators=[cors.crossdomain(origin='*', automatic_options=False)]
+api.methods = ['GET', 'OPTIONS', 'POST', 'PUT']
+
+# Takes a list of dict/json objects and add id field
+def index(l):
+    new_l = []
+    for i in range(len(l)):
+        new_x = l[i]
+        new_x['id'] = i
+        new_l.append(new_x)
+    return new_l
+
+nouns = index(json.load(open("assets/root_nouns_upd3-18.json")))
+verbs = index(json.load(open("assets/root_verbs_upd3-18.json")))
+postbases = index(json.load(open("assets/postbases_upd3-18.json")))
+endings = index(json.load(open("assets/endings_upd3-18.json")))
 
 class Nouns(Resource):
     def __init__(self, *args, **kwargs):
         super(Resource, self).__init__(*args, **kwargs)
-        self.nouns = json.load(open("assets/root_nouns_upd3-18.json"))
 
+    @cors.crossdomain(origin='*')
     def get(self):
-        return self.nouns
+        return jsonify(nouns)
 
 class Verbs(Resource):
     def __init__(self, *args, **kwargs):
         super(Resource, self).__init__(*args, **kwargs)
-        self.verbs = json.load(open("assets/root_verbs_upd3-18.json"))
 
+    @cors.crossdomain(origin='*')
     def get(self):
-        return self.verbs
+        return verbs
 
 class Postbases(Resource):
     def __init__(self, *args, **kwargs):
         super(Resource, self).__init__(*args, **kwargs)
-        self.postbases = json.load(open("assets/postbases_upd3-18.json"))
 
+    @cors.crossdomain(origin='*')
     def get(self):
-        return self.postbases
+        return postbases
 
 class Endings(Resource):
     def __init__(self, *args, **kwargs):
         super(Resource, self).__init__(*args, **kwargs)
-        self.endings = json.load(open("assets/endings_upd3-18.json"))
 
+    @cors.crossdomain(origin='*')
     def get(self):
-        return self.endings
+        return endings
 
 class Word(Resource):
+    @cors.crossdomain(origin='*')
     def get(self, word):
         print(word)
         return {'english': 'mother', 'yupik': 'aakaq'}
+
 
 api.add_resource(Word, '/word/<string:word>')
 api.add_resource(Nouns, '/noun/all')
