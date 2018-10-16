b0f89eca7b3dac4b903b0f142c5f4e2fa815e0f4
Temigo <temigo@gmx.com>
2018-10-16 13:18:18 -0700

    fix

--- api.py
+++ api.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 # app.wsgi_app.add_files('static/')
 app.config['FLASKS3_BUCKET_NAME'] = 'yugtun-static'
 app.config['FLASKS3_REGION'] = 'DEFAULT'
-app.config['FLASKS3_DEBUG'] = False
+app.config['FLASKS3_DEBUG'] = True
 app.config['FLASKS3_HEADERS'] = {
     'Cache-Control': 'max-age=86400',
 }
