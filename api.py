f30415bf4788bfbcdfea1b8b92e66d78f9b0fad5
Topher <christopher.w.liu@gmail.com>
2020-11-20 10:54:30 -0900

    added URI decoder

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ from flask_compress import Compress
 #from whitenoise import WhiteNoise
 from flask_s3 import FlaskS3, url_for
 import hfst
-
+from urllib import unquote
 
 app = Flask(__name__)
 # app.wsgi_app = WhiteNoise(app.wsgi_app)
@@ -121,7 +121,7 @@ class Word(Resource):
     @cors.crossdomain(origin='*')
     def get(self, word):
         print(word)
-        return jsonify(new_dict0[word])
+        return jsonify(new_dict0[unquote(word).decode('utf8')])
 
 
 class WordsList(Resource):
