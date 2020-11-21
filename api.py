d54f3ef60d0ca249f6e54440a047d0c862c658b0
Topher <christopher.w.liu@gmail.com>
2020-11-20 16:32:51 -0900

    added encode and decode

--- api.py
+++ api.py
@@ -120,8 +120,10 @@ class Word(Resource):
 
     @cors.crossdomain(origin='*')
     def get(self, word):
-        print(word.encode("utf-8"))
-        return jsonify(new_dict0[word.encode("utf-8")])
+        word = word.encode("utf-8")
+        word = word.decode("utf-8")
+        print(word)
+        return jsonify(new_dict0[word])
 
 
 class WordsList(Resource):
