4f77943604fd8f614fb205a7a651768333a5ee02
Topher <christopher.w.liu@gmail.com>
2020-11-20 16:59:56 -0900

    changed dict to utf8

--- api.py
+++ api.py
@@ -120,8 +120,8 @@ class Word(Resource):
 
     @cors.crossdomain(origin='*')
     def get(self, word):
-        print(word)
-        return jsonify(new_dict0[word])
+        print(word.encode("utf-8"))
+        return jsonify(new_dict0[word.encode("utf-8")])
 
 
 class WordsList(Resource):
